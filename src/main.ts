f1cfae85f540ea8433dd9696b872cbcbd8e6e32a
StanislavDevel <unfeel00@gmail.com>
2025-05-09 13:51:04 +0200

    feat: add common fetchToken utility and implement requestLoginToken & requestAnonymousToken in authClient.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import './assets/styles/app.scss';
 import '../node_modules/uikit/dist/js/uikit.min.js';
 import '../node_modules/uikit/dist/js/uikit-icons.js';
+import {
+  requestAnonymousToken,
+  requestBearerToken,
+  requestLoginToken,
+} from './clients/authClient.js';
 
-import { getToken } from './clients/authClient.js';
-
+// ф-я для демонстрации работы запросов токенов.
 async function init() {
-  const bearerToken = await getToken(); //  получили главный токен для дальнейших действий
-  console.log(bearerToken);
+  await requestBearerToken(); //  получили главный токен для дальнейших действий
+  await requestAnonymousToken();
+  await requestLoginToken('unfeel00@gmail.com', '123456');
 }
 
 document.addEventListener('DOMContentLoaded', init);
