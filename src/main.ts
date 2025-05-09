3cc4f64c66bf6c48d4ae7ba831225f80cad6cb62
Stanislav Tsibulskii <129120422+Quoralis@users.noreply.github.com>
2025-05-09 11:55:31 +0300

    Feature/auth service without sdk (#12)
    * chore: add 'type': 'module' to package.json and envDir '../' in vite.config.js
    
    * chore: add project configuration file
    
    * feat: add authorization function, types, and try/catch wrapper
    
    * chore: remove generated CSS and CSS.map files after disabling auto-compilation in WebStorm

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
 import './assets/styles/app.scss';
 import '../node_modules/uikit/dist/js/uikit.min.js';
 import '../node_modules/uikit/dist/js/uikit-icons.js';
+
+import { getToken } from './clients/authClient.js';
+
+async function init() {
+  const bearerToken = await getToken(); //  получили главный токен для дальнейших действий
+  console.log(bearerToken);
+}
+
+document.addEventListener('DOMContentLoaded', init);
