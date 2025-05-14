cf912cf1a02bbe928b910c9c8dc1f523ffc3224d
StanislavDevel <unfeel00@gmail.com>
2025-05-14 17:10:03 +0200

    feat:  - rewrite login function and add redirect to homepage                       - refactor `login` to use async/await                       - on successful login, save auth token and navigate to  main

--- src/components/header/burgerMenu.ts
+++ src/components/header/burgerMenu.ts
@@ -31,7 +31,7 @@ export const showBurgerMenu = (): void => {
     ],
     parent: burgerMenuWrapper,
   });
-  
+
   document.addEventListener('click', (event): void => {
     if (event.target && event.target instanceof HTMLElement) {
       if (burgerMenu.classList.contains('uk-offcanvas-slide')) {
--- src/components/mainPage/mainStructure.ts
+++ src/components/mainPage/mainStructure.ts
@@ -2,15 +2,14 @@ import { createEl } from '../../utils/createElement.js';
 import { showHeaderComponent } from '../header/headerStructure.js';
 import { showBurgerMenu } from '../header/burgerMenu.js';
 
-showHeaderComponent();
-showBurgerMenu();
-
 const mainPageWrapper = createEl({
   tag: 'div',
   classes: ['uk-height-1-1', 'main-page-wrapper'],
 });
 
 const showMainPage = (): void => {
+  showHeaderComponent();
+  showBurgerMenu();
   document.body.append(mainPageWrapper);
 };
 
