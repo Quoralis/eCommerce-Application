23cb6aa44daca3050ba2e8177de5656a0908aabd
StanislavDevel <unfeel00@gmail.com>
2025-05-25 17:58:34 +0200

    feat:  add func.  get categoryId  and  fetch all products in category

--- src/clients/categoriesClient.ts
+++ src/clients/categoriesClient.ts
@@ -4,3 +4,10 @@ import { Categories } from '../types/types.js';
 export async function fetchAllCategories(): Promise<Categories> {
   return await fetchFromApi('categories');
 }
+
+export async function getCategoriesId(key: string): Promise<string> {
+  const categoryId: { id: string } = await fetchFromApi(
+    `categories/key=${key}`
+  );
+  return categoryId.id;
+}
--- src/clients/getCurrentProductClient.ts
+++ src/clients/getCurrentProductClient.ts
@@ -1,5 +1,7 @@
 import { CurrentProduct, ProductsResponse } from '../types/types.js';
 import { fetchFromApi } from '../services/fetchFromApi.js';
+import { apiUrl, projectKey } from '../config.js';
+import { wrapperTryCatch } from '../utils/wrapperTryCatch.js';
 
 export const getCurrentProductClient = async (
   key: string
@@ -10,3 +12,27 @@ export const getCurrentProductClient = async (
 export const getAllProductsClients = async (): Promise<ProductsResponse> => {
   return await fetchFromApi(`product-projections`);
 };
+
+export async function getProductsInCategory(
+  bearerToken: string,
+  categoryId: string,
+  limit = 20,
+  offset = 0
+): Promise<[]> {
+  const url = `${apiUrl}/${projectKey}/product-projections/search`;
+
+  const form = new URLSearchParams();
+  form.append('filter', `categories.id:"${categoryId}"`);
+  form.append('limit', limit.toString());
+  form.append('offset', offset.toString());
+
+  const response = await wrapperTryCatch<{ results: [] }>(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Bearer ${bearerToken}`,
+    },
+    body: form.toString(),
+  });
+  return response.results;
+}
