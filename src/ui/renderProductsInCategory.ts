e2d65da33287ec77afcb9b7fc7a2eb6d311ea87e
StanislavDevel <unfeel00@gmail.com>
2025-06-15 19:22:41 +0200

    refactor: replaced 'as' with 'instanceof' for safer type checking

--- src/ui/renderProductsInCategory.ts
+++ src/ui/renderProductsInCategory.ts
@@ -10,52 +10,56 @@ export async function renderProductsInCategory(
   limit: number = 8,
   offset: number = 0
 ): Promise<void> {
-  const productWrapper = document.querySelector(
-    '.product-wrapper'
-  ) as HTMLElement;
-  productWrapper.innerHTML = '';
-  const productContainer = document.querySelector(
-    '.product-container'
-  ) as HTMLElement;
-  const categoryId = await getCategoriesId(keyCategory);
-  console.log(categoryId);
+  const productWrapper = document.querySelector('.product-wrapper');
+  const productContainer = document.querySelector('.product-container');
+  if (
+    productWrapper instanceof HTMLElement &&
+    productContainer instanceof HTMLElement
+  ) {
+    productWrapper.innerHTML = '';
+    const categoryId = await getCategoriesId(keyCategory);
+    console.log(categoryId);
 
-  const bearToken = localStorage.getItem('bearerToken');
-  if (bearToken && categoryId) {
-    const minInput = document.getElementById('min-price') as HTMLInputElement;
-    const maxInput = document.getElementById('max-price') as HTMLInputElement;
-    const hasPriceFilter =
-      minInput.value.trim() !== '' || maxInput.value.trim() !== '';
+    const bearToken = localStorage.getItem('bearerToken');
+    if (bearToken && categoryId) {
+      const minInput = document.getElementById('min-price');
+      const maxInput = document.getElementById('max-price');
+      if (
+        minInput instanceof HTMLInputElement &&
+        maxInput instanceof HTMLInputElement
+      ) {
+        const hasPriceFilter =
+          minInput.value.trim() !== '' || maxInput.value.trim() !== '';
 
-    const products: ProductsResponse = hasPriceFilter
-      ? await searchProductsPrice(categoryId, limit, offset)
-      : await getProductsInCategory(bearToken, categoryId, limit, offset);
+        const products: ProductsResponse = hasPriceFilter
+          ? await searchProductsPrice(categoryId, limit, offset)
+          : await getProductsInCategory(bearToken, categoryId, limit, offset);
 
-    console.log(products);
+        console.log(products);
 
-    const currentPage = Math.floor(offset / limit) + 1;
-    renderPagination(
-      productContainer,
-      products.total || products.count,
-      currentPage,
-      (page) => {
-        const newOffset = (page - 1) * limit;
-        renderProductsInCategory(keyCategory, limit, newOffset);
+        const currentPage = Math.floor(offset / limit) + 1;
+        renderPagination(
+          productContainer,
+          products.total || products.count,
+          currentPage,
+          (page) => {
+            const newOffset = (page - 1) * limit;
+            renderProductsInCategory(keyCategory, limit, newOffset);
+          }
+        );
+        products.results.forEach((product) => {
+          const dataCard: DisplayProduct = {
+            productName: product.name.en,
+            imageUrl: product.masterVariant.images![0].url,
+            productKey: product.key,
+            description: product.description.en,
+            price: product.masterVariant.prices[0].value.centAmount,
+            discountedPrice:
+              product.masterVariant.prices[0].discounted?.value.centAmount,
+          };
+          renderProductCard(productWrapper, dataCard, keyCategory);
+        });
       }
-    );
-
-    products.results.forEach((product) => {
-      const dataCard: DisplayProduct = {
-        productName: product.name.en,
-        imageUrl: product.masterVariant.images![0].url,
-        productKey: product.key,
-        description: product.description.en,
-        price: product.masterVariant.prices[0].value.centAmount,
-        discountedPrice:
-          product.masterVariant.prices[0].discounted?.value.centAmount,
-      };
-      renderProductCard(productWrapper, dataCard, keyCategory);
-    });
-  } else {
+    }
   }
 }
